1df3d89754fcbfc057ce2a23a323f42265443f49
stilrdim <stilrdim@gmail.com>
2021-11-05 18:20:58 +0000

    Initial commit

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ file_name = 'songs.txt'
 with open(file_name, 'r+') as f:
     songs = f.readlines()
 
-download_all(songs=songs, new_folder="\\test2")
+download_all(songs=songs, new_folder=r"\test")
